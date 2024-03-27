306913021fe614fa9030e6aca0adc629d9382b80
nyitraibence <nyitrai100@gmail.com>
2024-03-27 07:53:38 +0000

    .

--- Program.cs
+++ Program.cs
@@ -212,3 +212,40 @@ to update the value. The code then prints the updated value to the console.
 //     value = 10;
 //     return value;
 // }
+
+
+  //Access the properties of an exception object
+  
+  try
+  {
+      Process1();
+  }
+  catch
+  {
+      Console.WriteLine("An exception has occurred");
+  }
+
+  Console.WriteLine("Exit program");
+
+  static void Process1()
+  {
+      try
+      {
+          WriteMessage();
+      }
+      catch (Exception ex)
+      {
+          Console.WriteLine($"Exception caught in Process1: {ex.Message}");
+      }
+  }
+
+  static void WriteMessage()
+  {
+      double float1 = 3000.0;
+      double float2 = 0.0;
+      int number1 = 3000;
+      int number2 = 0;
+
+      Console.WriteLine(float1 / float2);
+      Console.WriteLine(number1 / number2);
+  }
